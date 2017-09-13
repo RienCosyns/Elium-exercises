e72c05fcec19d322dd86696ffdf30556f3cc4a21
RienCosyns <rien.cosyns@gmail.com>
2017-09-13 18:10:34 +0200

    Update 23_alternating_cases_solution.js

--- Solutions/week 1/day2_arrays_and_functions/23_alternating_cases_solution.js
+++ Solutions/week 1/day2_arrays_and_functions/23_alternating_cases_solution.js
@@ -26,5 +26,26 @@ function alternating(string) {
 
 }
 
+// much cooler solution
+function alternating(string) {
+  var newstring = "";
+  for (var i = 0; i < string.length; i++) {
+    // if lowercase make uppercase, if upper make lower
+    if (string[i].charCodeAt(0) >= 65 && string[i].charCodeAt(0) <= 90) {
+      var char = string[i].toLowerCase();
+      newstring += char;
+    } else if (
+      string[i].charCodeAt(0) >= 97 &&
+      string[i].charCodeAt(0) <= 122
+    ) {
+      var char = string[i].toUpperCase();
+      newstring += char;
+    } else {
+      newstring += string[i];
+    }
+  }
+  return newstring;
+}
+
 console.log(alternating('I am Prajjwal and I do everything wrong'))
 // outputs i AM pRAJJWAL AND i DO EVERYTHING WRONG
